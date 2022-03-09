75c82e1a733b26725e092e0ccabed6efbd641819
Yilmar Ospina <ospina.yilmar@gmail.com>
2022-03-09 13:38:36 -0500

    Estructura de los componentes TODO creada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+
+// import logo from './logo.svg';
+// import './App.css';
+
+const todos = [
+  {text: 'Reparar Macbook de Germán', completed: false},
+  {text: 'Llevar escalera a la normal', completed: false},
+  {text: 'Hablar con Carlos sobre impresoras para comprar', completed: false},
+  {text: 'Hablar con Carlos sobre impresoras', completed: false},
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+
+      <TodoSearch />
+
+      <TodoList>
+        {todos.map(todo => (
+            <TodoItem key={todo.text} text={todo.text} />
+          ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
---
+++ src/TodoSearch.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoSearch() {
+    return(
+      <input placeholder="Macbook Pro de 13''" />
+    );
+}
+
+export { TodoSearch };
