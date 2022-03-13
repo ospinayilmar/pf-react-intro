d384d3d3d52d81ad1b4a48cd2d235a2a5218197e
Yilmar Ospina <ospina.yilmar@gmail.com>
2022-03-13 08:49:10 -0500

    Se crearon funciones para completar y eliminar tareas de la lista, estas si se crearon como funciones y se llamaron desde el componente todoItem

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,26 +2,18 @@ import React from "react";
 import './TodoItem.css';
 
 function TodoItem(props) {
-    const onComplete = () => {
-        alert('Tarea completada ' + props.text);
-    };
-    
-    const onDelete = () => {
-        alert('Tarea eliminada ' + props.text);
-    };
-
     return (
         <li className="TodoItem">
             <span 
                 className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-                onClick={onComplete}
+                onClick={props.onComplete}
             >
                 √
             </span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
             <span
                 className="Icon Icon-delete"
-                onClick={onDelete}
+                onClick={props.onDelete}
             >
                 X
             </span>
