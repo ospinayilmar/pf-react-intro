d82982eafd70004c3a6fff5813ddab3515c29535
Yilmar Ospina <ospina.yilmar@gmail.com>
2022-03-12 20:44:07 -0500

    Se usaron los estados para filtrar la lista de todo según lo escrito en el buscador y se usaron funciones de manejo de cadenas para convertir los textos a minúsculas

--- src/App.js
+++ src/App.js
@@ -7,22 +7,49 @@ import { CreateTodoButton } from "./CreateTodoButton";
 
 // import './App.css';
 
-const todos = [
+const defaultTodos = [
   {text: 'Reparar Macbook de Germán', completed: false},
-  {text: 'Llevar escalera a la normal', completed: true},
+  {text: 'Llevar escalera a la normal', completed: false},
   {text: 'Hablar con Carlos sobre impresoras para comprar', completed: false},
   {text: 'Hablar con Carlos sobre impresoras', completed: true},
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos =  todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if(searchValue.length <= 0){
+    searchedTodos = todos;
+  }
+  else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+
+      return todoText.includes(searchText);
+    });
+  }
+
+
   return (
     <React.Fragment>
-      <TodoCounter />
+      <TodoCounter
+        total={totalTodos}
+        completed={completedTodos}
+      />
 
-      <TodoSearch />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
             <TodoItem 
               key={todo.text}
               text={todo.text}
