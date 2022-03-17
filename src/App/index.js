0ec9c3ba0766e37b8b93512be9de6b299cd9d871
Yilmar Ospina <ospina.yilmar@gmail.com>
2022-03-16 21:01:35 -0500

    Se comentó el arreglo de pruebas de llenar el localStorage para evitar conflictos en el código

--- src/App/index.js
+++ src/App/index.js
@@ -12,3 +12,11 @@ function App() {
 }
 
 export default App;
+
+
+// const defaultTodos = [ 
+//   {text: 'Reparar Macbook de Germán', completed: false}, 
+//   {text: 'Llevar escalera a la normal', completed: false}, 
+//   {text: 'Hablar con Carlos sobre impresoras para comprar', completed: false}, 
+//   {text: 'Hablar con Carlos sobre impresoras', completed: true}, 
+// ];
