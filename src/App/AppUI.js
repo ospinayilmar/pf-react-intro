f3dd405352658ba0a403adea4f4f321206e20127
Yilmar Ospina <ospina.yilmar@gmail.com>
2022-06-23 21:32:50 -0400

    Se actualizó el código a la lección 17 Se implementó useContext para consumir el contexto Se corrigieron los errores en el contador y en el buscador

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { TodoContext } from '../Todocontext';
 import { TodoCounter } from "../TodoCounter";
 import { TodoSearch } from "../TodoSearch";
@@ -7,36 +7,34 @@ import { TodoItem } from "../TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton";
 
 function AppUI() {
+    const { 
+        error,
+        loading, 
+        searchedTodos, 
+        completeTodo, 
+        deleteTodo 
+    } = useContext(TodoContext);
+
     return (
         <React.Fragment>
             <TodoCounter />
             <TodoSearch />
             
-            <TodoContext.Consumer>
-                {({ 
-                    error,
-                    loading, 
-                    searchedTodos, 
-                    completeTodo, 
-                    deleteTodo 
-                }) => (
-                    <TodoList>
-                        {error && <p>Desespérate, hubo un error.</p>}
-                        {loading && <p>No desesperes, estamos cargando.</p>}
-                        {(!loading && !searchedTodos.length) && <p>Crea tu primer TODO.</p>}
-        
-                        {searchedTodos.map(todo => (
-                            <TodoItem 
-                                key={todo.text}
-                                text={todo.text}
-                                completed = {todo.completed}
-                                onComplete={() => completeTodo(todo.text)}
-                                onDelete={() => deleteTodo(todo.text)}
-                            />
-                        ))}
-                    </TodoList>
-                )}
-            </TodoContext.Consumer>
+            <TodoList>
+                {error && <p>Desespérate, hubo un error.</p>}
+                {loading && <p>No desesperes, estamos cargando.</p>}
+                {(!loading && !searchedTodos.length) && <p>Crea tu primer TODO.</p>}
+
+                {searchedTodos.map(todo => (
+                    <TodoItem 
+                        key={todo.text}
+                        text={todo.text}
+                        completed = {todo.completed}
+                        onComplete={() => completeTodo(todo.text)}
+                        onDelete={() => deleteTodo(todo.text)}
+                    />
+                ))}
+            </TodoList>
 
             <CreateTodoButton />
         </React.Fragment>
--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { TodoContext } from "../Todocontext";
 import './TodoSearch.css';
 
-function TodoSearch({ searchValue, setSearchValue }) {
+function TodoSearch() {
+  const { searchValue, setSearchValue } = React.useContext(TodoContext);
+
   const onSearchValueChange = (event) => {
     console.log("El cajón cambió " + event.target.value);
     setSearchValue(event.target.value);
   };
+
     return(
       <React.Fragment>
         <input 
